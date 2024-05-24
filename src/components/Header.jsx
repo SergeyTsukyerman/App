ab9a028021543284c3b8d1841c9337ea5a051f86
Sergey Tsukyerman <ts.sergey78@gmail.com>
2024-05-24 16:19:18 +0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,18 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-slate-200 shadow-md ">
-      <div className="flex justify-between items-center">
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-around items-center">
         <h1 className="font-bold">
           <span className="text-blue-500 text-4xl">My</span>
           <span className="text-gray-600 text-4xl">Drone</span>
         </h1>
-        <form>
-          <input type="text" placeholder="Search..." />
+        <form className="bg-slate-100 p-3 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent"
+          />
         </form>
       </div>
     </header>
